c074de3b65c2f94efce97725b664eb6f4225c79d
Alessandro Nardini <alexnardini@gmail.com>
2021-05-19 05:29:02 -0400

    Added GNU General Public License msg to all header's files

--- src/genome.h
+++ src/genome.h
@@ -16,6 +16,21 @@
  /  Comment:    GENOME entities and properties.
 */
 
+/*
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 struct genomeSYS{
     int TMG, DELINVALIDPT, POSTF, FF, iter_f, symmetry, sym_mode, sym_global, iter, domb, vizmb;
     float mb_modulate;
