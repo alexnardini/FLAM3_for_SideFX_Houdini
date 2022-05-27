9e2f22ab538e71d04dde06a8e6b95ea9dd5ee8d2
Alessandro Nardini <alexnardini@gmail.com>
2022-05-27 16:15:18 -0400

    Lower case idx

--- src/genome.h
+++ src/genome.h
@@ -55,58 +55,58 @@ struct gem{
             if(!ACTV[i]) continue;
             res++;
             // Collect active variation IDs
-            string IDX=itoa(i+1);
-            append(sIDX, IDX);
+            string idx=itoa(i+1);
+            append(sIDX, idx);
             // Color
-            clr = chf(concat("../clr_", IDX));
-            speed = chf(concat("../clrspeed_", IDX));
+            clr = chf(concat("../clr_", idx));
+            speed = chf(concat("../clrspeed_", idx));
             append(CLR, speed*clr);
             append(ONEMINUS, 1-speed);
-            append(ALPHA, chf(concat("../alpha_", IDX)));
+            append(ALPHA, chf(concat("../alpha_", idx)));
             // PRE BLUR
-            append(pbw, chf(concat("../preblurweight_" , IDX)));
+            append(pbw, chf(concat("../preblurweight_" , idx)));
             // PRE VAR 01
-            append(ppw, chf(concat("../pre1weight_" , IDX)));
-            if(ppw[-1]>0) append(pptype, atoi(chs(concat("../pre1type_", IDX))));
+            append(ppw, chf(concat("../pre1weight_" , idx)));
+            if(ppw[-1]>0) append(pptype, atoi(chs(concat("../pre1type_", idx))));
             else resize(pptype, res);
             // VAR 01
-            append(v1w, chf(concat("../v1weight_", IDX)));
-            if(v1w[-1]!=0) append(v1type, atoi(chs(concat("../v1type_", IDX))));
+            append(v1w, chf(concat("../v1weight_", idx)));
+            if(v1w[-1]!=0) append(v1type, atoi(chs(concat("../v1type_", idx))));
             else resize(v1type, res);
             // VAR 02
-            append(v2w, chf(concat("../v2weight_", IDX)));
-            if(v2w[-1]!=0) append(v2type, atoi(chs(concat("../v2type_", IDX))));
+            append(v2w, chf(concat("../v2weight_", idx)));
+            if(v2w[-1]!=0) append(v2type, atoi(chs(concat("../v2type_", idx))));
             else resize(v2type, res);
             // VAR 03
-            append(v3w, chf(concat("../v3weight_", IDX)));
-            if(v3w[-1]!=0) append(v3type, atoi(chs(concat("../v3type_", IDX))));
+            append(v3w, chf(concat("../v3weight_", idx)));
+            if(v3w[-1]!=0) append(v3type, atoi(chs(concat("../v3type_", idx))));
             else resize(v3type, res);
             // VAR 04
-            append(v4w, chf(concat("../v4weight_", IDX)));
-            if(v4w[-1]!=0) append(v4type, atoi(chs(concat("../v4type_", IDX))));
+            append(v4w, chf(concat("../v4weight_", idx)));
+            if(v4w[-1]!=0) append(v4type, atoi(chs(concat("../v4type_", idx))));
             else resize(v4type, res);
             // // POST VAR 01
-            append(p1w, chf(concat("../p1weight_", IDX)));
-            if(p1w[-1]!=0) append(p1type, atoi(chs(concat("../p1type_", IDX))));
+            append(p1w, chf(concat("../p1weight_", idx)));
+            if(p1w[-1]!=0) append(p1type, atoi(chs(concat("../p1type_", idx))));
             else resize(p1type, res);
             // Collect affine coefficients
-            _x = chu(concat("../x_", IDX));
-            _y = chu(concat("../y_", IDX));
-            _a = chf(concat("../ang_", IDX));
+            _x = chu(concat("../x_", idx));
+            _y = chu(concat("../y_", idx));
+            _a = chf(concat("../ang_", idx));
             affineRot(_m2, _x, _y, -radians(_a));
             append(x, set(_m2.xx, _m2.xy));
             append(y, set(_m2.yx, _m2.yy));
-            append(o, chu(concat("../o_", IDX)));
+            append(o, chu(concat("../o_", idx)));
             // POST
-            append(POSTL, chi(concat("../dopost_", IDX)));
+            append(POSTL, chi(concat("../dopost_", idx)));
             if(POSTL[-1]){
-                _x = chu(concat("../px_", IDX));
-                _y = chu(concat("../py_", IDX));
-                _a = chf(concat("../pang_", IDX));
+                _x = chu(concat("../px_", idx));
+                _y = chu(concat("../py_", idx));
+                _a = chf(concat("../pang_", idx));
                 affineRot(_m2, _x, _y, -radians(_a));
                 append(px, set(_m2.xx, _m2.xy));
                 append(py, set(_m2.yx, _m2.yy));
-                append(po, chu(concat("../po_", IDX)));
+                append(po, chu(concat("../po_", idx)));
                 }
             else{ resize(px, res); resize(py, res); resize(po, res); }
         }
@@ -162,7 +162,7 @@ struct gemPrm{
 
         if(max(GEMTYPE)>26){
             int TYPE;
-            string IDX;
+            string idx;
             // FLOAT
             resize(rings2_val, res); bipolar_shift=cell_size=escher_beta=popcorn2_c=flux_spread=rings2_val;
             // VECTOR
@@ -173,37 +173,37 @@ struct gemPrm{
             resize(ngon, res);       pdj_w=oscope=wedge=wedgejulia=wedgesph=auger=mobius_re=mobius_im=ngon;
 
             for(int i=0; i<res; ++i){
-                TYPE=GEMTYPE[i]; IDX=sIDX[i];
+                TYPE=GEMTYPE[i]; idx=sIDX[i];
                 if(TYPE<27 || w[i]==0) continue;
                 else if(find( {27, 28, 29, 30, 31, 32, 34, 35, 36, 37, 38, 47, 48, 49, 50, 51, 52, 53, 56, 57} , TYPE)>=0){
                     if(TYPE<38){
                         // 27 CURL
-                        if(TYPE==27){ curl_c[i] = chu(concat("../curlc_", IDX)); continue; }
+                        if(TYPE==27){ curl_c[i] = chu(concat("../curlc_", idx)); continue; }
                         // 28 NGON
-                        else if(TYPE==28){ ngon[i]  = chp(concat("../ngon_", IDX)); continue; }
+                        else if(TYPE==28){ ngon[i]  = chp(concat("../ngon_", idx)); continue; }
                         // 29 PDJ
-                        else if(TYPE==29){ pdj_w[i] = chp(concat("../pdjw_", IDX)); continue; }
+                        else if(TYPE==29){ pdj_w[i] = chp(concat("../pdjw_", idx)); continue; }
                         // 30 BLOB
-                        else if(TYPE==30){ blob[i]  = chv(concat("../blob_", IDX)); continue; }
+                        else if(TYPE==30){ blob[i]  = chv(concat("../blob_", idx)); continue; }
                         // 31 JuliaN
-                        else if(TYPE==31){ julian[i] = chu(concat("../julian_", IDX)); continue; }
+                        else if(TYPE==31){ julian[i] = chu(concat("../julian_", idx)); continue; }
                         // 32 JuliaScope
-                        else if(TYPE==32){ juliascope[i] = chu(concat("../juliascope_", IDX)); continue; }
+                        else if(TYPE==32){ juliascope[i] = chu(concat("../juliascope_", idx)); continue; }
                         // 34 FAN2
-                        else if(TYPE==34){ fan2[i] = chu(concat("../fan2_", IDX)); continue; }
+                        else if(TYPE==34){ fan2[i] = chu(concat("../fan2_", idx)); continue; }
                         // 35 RINGS2
-                        else if(TYPE==35){ rings2_val[i] = chf(concat("../rings2val_", IDX)); continue; }
+                        else if(TYPE==35){ rings2_val[i] = chf(concat("../rings2val_", idx)); continue; }
                         // 36 RECTANGLES
-                        else if(TYPE==36){ rectangles[i] = chu(concat("../rectangles_", IDX)); continue; }
+                        else if(TYPE==36){ rectangles[i] = chu(concat("../rectangles_", idx)); continue; }
                         // 37 RADIAL BLUR
-                        else if(TYPE==37){ radialblur[i] = chu(concat("../radialblur_", IDX)); continue; }
+                        else if(TYPE==37){ radialblur[i] = chu(concat("../radialblur_", idx)); continue; }
                     }
                     else{
                         // 38 PIE
-                        if(TYPE==38){ pie[i] = chv(concat("../pie_", IDX)); continue; }
+                        if(TYPE==38){ pie[i] = chv(concat("../pie_", idx)); continue; }
                         // 47 DISC2 ( This seem to be the only one to benefit from the precalc: 18% faster. )
                         else if(TYPE==47){
-                            disc2[i] = chu(concat("../disc2_", IDX));
+                            disc2[i] = chu(concat("../disc2_", idx));
                             float rot=disc2[i][0]; float twist=disc2[i][1];
                             vector calc; precalc_V_DISC2(calc, rot, twist);
                             pc_DISC2[i] = calc;
@@ -211,54 +211,54 @@ struct gemPrm{
                         }
                         // 48 SUPERSHAPE
                         else if(TYPE==48){
-                            supershape[i]   = chv(concat("../supershape_", IDX));
-                            supershape_n[i] = chv(concat("../supershapen_", IDX)); continue; }
+                            supershape[i]   = chv(concat("../supershape_", idx));
+                            supershape_n[i] = chv(concat("../supershapen_", idx)); continue; }
                         // 49 FLOWER
-                        else if(TYPE==49){ flower[i] = chu(concat("../flower_", IDX)); continue; }
+                        else if(TYPE==49){ flower[i] = chu(concat("../flower_", idx)); continue; }
                         // 50 CONIC
-                        else if(TYPE==50){ conic[i] = chu(concat("../conic_", IDX)); continue; }
+                        else if(TYPE==50){ conic[i] = chu(concat("../conic_", idx)); continue; }
                         // 51 PARABOLA
-                        else if(TYPE==51){ parabola[i] = chu(concat("../parabola_", IDX)); continue; }
+                        else if(TYPE==51){ parabola[i] = chu(concat("../parabola_", idx)); continue; }
                         // 52 BENT2
-                        else if(TYPE==52){ bent2[i] = chu(concat("../bent2xy_", IDX)); continue; }
+                        else if(TYPE==52){ bent2[i] = chu(concat("../bent2xy_", idx)); continue; }
                         // 53 BIPOLAR
-                        else if(TYPE==53){ bipolar_shift[i] = chf(concat("../bipolarshift_", IDX)); continue; }
+                        else if(TYPE==53){ bipolar_shift[i] = chf(concat("../bipolarshift_", idx)); continue; }
                         // 56 CELL
-                        else if(TYPE==56){ cell_size[i] = chf(concat("../cellsize_", IDX)); continue; }
+                        else if(TYPE==56){ cell_size[i] = chf(concat("../cellsize_", idx)); continue; }
                         // 57 CPOW
-                        else if(TYPE==57){ cpow[i] = chv(concat("../cpow_", IDX)); continue; }
+                        else if(TYPE==57){ cpow[i] = chv(concat("../cpow_", idx)); continue; }
                     }
                 }
                 else if(find( {61, 63, 66, 67, 69, 71, 72, 73, 74, 75, 76, 77, 78, 79, 94, 95, 96, 97, 98, 99, 101} , TYPE)>=0){
                     if(TYPE<77){
                         // 61 ESCHER
-                        if(TYPE==61){ escher_beta[i] = chf(concat("../escherbeta_", IDX)); continue; }
+                        if(TYPE==61){ escher_beta[i] = chf(concat("../escherbeta_", idx)); continue; }
                         // 63 LAZYSUSAN
                         else if(TYPE==63){
-                            lazysusanxyz[i] = chu(concat("../lazysusanxyz_", IDX));
-                            lazysusan[i]    = chv(concat("../lazysusan_", IDX)); continue; }
+                            lazysusanxyz[i] = chu(concat("../lazysusanxyz_", idx));
+                            lazysusan[i]    = chv(concat("../lazysusan_", idx)); continue; }
                         // 66 MODULUS
-                        else if(TYPE==66){ modulus[i] = chu(concat("../modulusXYZ_", IDX)); continue; }
+                        else if(TYPE==66){ modulus[i] = chu(concat("../modulusXYZ_", idx)); continue; }
                         // 67 OSCOPE
-                        else if(TYPE==67){ oscope[i] = chp(concat("../oscope_", IDX)); continue; }
+                        else if(TYPE==67){ oscope[i] = chp(concat("../oscope_", idx)); continue; }
                         // 69 POPCORN2
                         else if(TYPE==69){
-                            popcorn2[i]   = chu(concat("../popcorn2xyz_", IDX));
-                            popcorn2_c[i] = chf(concat("../popcorn2c_", IDX)); continue; }
+                            popcorn2[i]   = chu(concat("../popcorn2xyz_", idx));
+                            popcorn2_c[i] = chf(concat("../popcorn2c_", idx)); continue; }
                         // 71 SEPARATION
                         else if(TYPE==71){
-                            separation[i]        = chu(concat("../separationxyz_", IDX));
-                            separation_inside[i] = chu(concat("../separationinsidexyz_", IDX)); continue; }
+                            separation[i]        = chu(concat("../separationxyz_", idx));
+                            separation_inside[i] = chu(concat("../separationinsidexyz_", idx)); continue; }
                         // 72 SPLIT
-                        else if(TYPE==72){ split[i] = chu(concat("../splitxyz_", IDX)); continue; }
+                        else if(TYPE==72){ split[i] = chu(concat("../splitxyz_", idx)); continue; }
                         // 73 SPLITS
-                        else if(TYPE==73){ splits[i] = chu(concat("../splitsxyz_", IDX)); continue; }
+                        else if(TYPE==73){ splits[i] = chu(concat("../splitsxyz_", idx)); continue; }
                         // 74 STRIPES
-                        else if(TYPE==74){ stripes[i] = chu(concat("../stripes_", IDX)); continue; }
+                        else if(TYPE==74){ stripes[i] = chu(concat("../stripes_", idx)); continue; }
                         // 75 WEDGE
-                        else if(TYPE==75){ wedge[i] = chp(concat("../wedge_", IDX)); continue; }
+                        else if(TYPE==75){ wedge[i] = chp(concat("../wedge_", idx)); continue; }
                         // 76 WEDGE JULIA ( The precalc made it 10% slower, I dnt know why...odd. )
-                        else if(TYPE==76){ wedgejulia[i] = chp(concat("../wedgejulia_", IDX));
+                        else if(TYPE==76){ wedgejulia[i] = chp(concat("../wedgejulia_", idx));
                             // precalc
                             // float power=wedgejulia[i][0]; float angle= wedgejulia[i][1]; float dist=wedgejulia[i][2]; float count=wedgejulia[i][3];
                             // vector calc; precalc_V_WEDGEJULIA(calc, power, angle, dist, count);
@@ -268,31 +268,31 @@ struct gemPrm{
                     }
                     else{
                         // 77 WEDGE SPH
-                        if(TYPE==77){ wedgesph[i] = chp(concat("../wedgesph_", IDX)); continue; }
+                        if(TYPE==77){ wedgesph[i] = chp(concat("../wedgesph_", idx)); continue; }
                         // 78 WHORL
-                        else if(TYPE==78){ whorl[i]  = chu(concat("../whorl_", IDX)); continue; }
+                        else if(TYPE==78){ whorl[i]  = chu(concat("../whorl_", idx)); continue; }
                         // 79 WAVES2
                         else if(TYPE==79){
-                            waves2_scale[i] = chu(concat("../waves2scalexyz_", IDX));
-                            waves2_freq[i]  = chu(concat("../waves2freqxyz_", IDX)); continue; }
+                            waves2_scale[i] = chu(concat("../waves2scalexyz_", idx));
+                            waves2_freq[i]  = chu(concat("../waves2freqxyz_", idx)); continue; }
                         // 94 AUGER
-                        else if(TYPE==94){ auger[i] = chp(concat("../auger_", IDX)); continue; }
+                        else if(TYPE==94){ auger[i] = chp(concat("../auger_", idx)); continue; }
                         // 95 FLUX
-                        else if(TYPE==95){ flux_spread[i] = chf(concat("../fluxspread_", IDX)); continue; }
+                        else if(TYPE==95){ flux_spread[i] = chf(concat("../fluxspread_", idx)); continue; }
                         // 96 MOBIUS
                         else if(TYPE==96){
-                            mobius_re[i] = chp(concat("../mobiusre_", IDX));
-                            mobius_im[i] = chp(concat("../mobiusim_", IDX)); continue; }
+                            mobius_re[i] = chp(concat("../mobiusre_", idx));
+                            mobius_im[i] = chp(concat("../mobiusim_", idx)); continue; }
                         // 97 CURVE
                         else if(TYPE==97){
-                            curve_lenght[i] = chu(concat("../curvexyzlenght_", IDX));
-                            curve_amp[i]    = chu(concat("../curvexyzamp_", IDX)); continue; }
+                            curve_lenght[i] = chu(concat("../curvexyzlenght_", idx));
+                            curve_amp[i]    = chu(concat("../curvexyzamp_", idx)); continue; }
                         // 98 PERSPECTIVE
-                        else if(TYPE==98){ persp[i] = chu(concat("../persp_", IDX)); continue; }
+                        else if(TYPE==98){ persp[i] = chu(concat("../persp_", idx)); continue; }
                         // 99 BWRAPS ( The precalc made it 15% slower, I dnt know why...odd. )
                         else if(TYPE==99){
-                            bwraps[i] = chv(concat("../bwraps_", IDX));
-                            bwrapstwist[i] = chu(concat("../bwrapstwist_", IDX));
+                            bwraps[i] = chv(concat("../bwraps_", idx));
+                            bwrapstwist[i] = chu(concat("../bwrapstwist_", idx));
                             // precalc
                             // float cellsize=bwraps[i][0]; float space=bwraps[i][1]; float gain=bwraps[i][2];
                             // vector calc; precalc_V_BWRAPS(calc, cellsize, space, gain);
@@ -300,9 +300,9 @@ struct gemPrm{
                             continue; }
                         // 101 POLYNOMIAL
                         else if(TYPE==101){
-                            polynomial_pow[i] = chu(concat("../polynomialpow_", IDX));
-                            polynomial_lc[i]  = chu(concat("../polynomiallc_", IDX));
-                            polynomial_sc[i]  = chu(concat("../polynomialsc_", IDX)); continue;
+                            polynomial_pow[i] = chu(concat("../polynomialpow_", idx));
+                            polynomial_lc[i]  = chu(concat("../polynomiallc_", idx));
+                            polynomial_sc[i]  = chu(concat("../polynomialsc_", idx)); continue;
                         }
                     }
                 }
