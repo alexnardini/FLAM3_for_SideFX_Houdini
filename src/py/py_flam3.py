00d8197b9e80dbf6241eca69ea78259c0db8af87
Alessandro Nardini <alexnardini@gmail.com>
2023-10-23 09:58:47 -0400

    Updated

--- src/py/py_flam3.py
+++ src/py/py_flam3.py
@@ -2124,7 +2124,7 @@ iterator_keep_last_weight(self) -> None:
         id = self.kwargs['script_multiparm_index']
         if len(W) == len(vactive_iters):
             node.setParms({f"{flam3h_iterator_prm_names.main_weight}_{str(id)}": min_weight})
-            print(f"{str(node)}: iterator {str(id)} Weight reverted back to being: {min_weight} instead of Zero.\nThere must always be at least one iterator weight above Zero.\n") # type: ignore
+            print(f"{str(node)}: iterator {str(id)}'s Weight reverted back to a value of: {min_weight} instead of Zero.\nThere must always be at least one active iterator's weight above Zero.\n") # type: ignore
 
 
 
