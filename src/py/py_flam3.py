9eca3c9368fae8c0c4a168ac8f010b215d2479e4
Alessandro Nardini <alexnardini@gmail.com>
2023-10-25 04:24:08 -0400

    Updated

--- src/py/py_flam3.py
+++ src/py/py_flam3.py
@@ -682,6 +682,8 @@ flam3h_on_loaded(self) -> None:
         """
         node = self.node
         
+        # We can skip to initialize xaos as the history is stored in the node itself.
+        
         # This is important so loading a hip file with a FLAM3H node
         # it wont block the houdini session until user input.
         self.flam3h_check_first_node_instance_msg(node, False)
