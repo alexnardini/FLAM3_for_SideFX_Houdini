c6de6e071d5ff74ff6e7f93336343a7c893c1da2
Alessandro Nardini <alexnardini@gmail.com>
2023-11-08 17:33:32 -0500

    Updated header file

--- src/cvex/genome.h
+++ src/cvex/genome.h
@@ -2,6 +2,10 @@
 #define __genome_h__
 
 /*  
+ /  Tested on:  Houdini 19.x
+ /              Houdini 19.5
+ /              Houdini 20.x
+ /
  /  Title:      SideFX Houdini FLAM3: 2D
  /  Author:     Alessandro Nardini
  /  date:       October 2020, Last revised February 2023
