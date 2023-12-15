fa8476ff52fce157c6d4558948114670fbd36ed6
Alessandro Nardini <alexnardini@gmail.com>
2023-12-15 03:47:36 -0500

    Updated

--- src/py/py_flam3_UI_PRM_map.py
+++ src/py/py_flam3_UI_PRM_map.py
@@ -332,6 +332,11 @@ hou.pwd().hdaModule().flam3.in_flame_utils(kwargs).set_iter_on_load_callback()
 hou.pwd().hdaModule().flam3.in_flame_utils(kwargs).use_iter_on_load_callback()
 
 
+'parameter name:    remappgb'
+'script type:       Menu Script'
+kwargs['node'].hdaModule().flam3.in_flame_utils(kwargs).in_to_flam3h_toggle("remappgb")
+
+
 'parameter name:    cprendervals'
 'script type:       Callback Script'
 hou.pwd().hdaModule().flam3.in_flame_utils.in_copy_render_stats_msg(kwargs)
