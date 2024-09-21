2526507f5883a548d8271f57a00d904053bc30c1
Alessandro Nardini <alexnardini@gmail.com>
2024-09-21 14:51:33 +0200

    v1.4.31 indie - updated Doc and About tab web link section

--- src/py/py_flam3_UI_PRM_map.py
+++ src/py/py_flam3_UI_PRM_map.py
@@ -32,7 +32,7 @@ flam3 = toolutils.createModuleFromSection("flam3", kwargs["type"], "py_flam3")
 """
 Inside: OTL->type_properties->Scripts->PreFirstCreate
 """
-FLAM3H_VERSION = '1.4.30 - Gold'
+FLAM3H_VERSION = '1.4.31 - Gold'
 
 def flam3h_first_time() -> None:
     hou_version = int(''.join(str(x) for x in hou.applicationVersion()[:1]))
