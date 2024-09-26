fbb6fdf8e72a2f52e2cb2959d9eddb331e53504a
Alessandro Nardini <alexnardini@gmail.com>
2024-09-26 21:24:11 +0200

    Added new high tier menu entrie icon.

--- src/py/py_flam3.py
+++ src/py/py_flam3.py
@@ -283,6 +283,8 @@ FLAM3H_ICON_STAR_FLAME_VAR_ACTV_OVER_ONE = '![opdef:/alexnardini::Sop/FLAM3H?ico
 FLAM3H_ICON_STAR_FLAME_VAR_ACTV_NEGATIVE = '![opdef:/alexnardini::Sop/FLAM3H?iconStarSwapCyanSVG.svg]'
 FLAM3H_ICON_STAR_FLAME_ITER_ACTV = '![opdef:/alexnardini::Sop/FLAM3H?icon_optionStarYellowOrangeSVG.svg]'
 
+# High tier menu entrie
+FLAM3H_ICON_STAR_HIGH_TIER = '![opdef:/alexnardini::Sop/FLAM3H?icon_optionStarRedHighSVG.svg]'
 
 
 
@@ -3172,7 +3174,7 @@ iterator_keep_last_weight(self) -> None:
         menu=[]
         menuitems = ()
         if iterators:
-            menuitems = ( "", "1M", "2M", "5M", "15M", "25M", "50M", "100M", "150M", "250M", f"{FLAM3H_ICON_COPY_PASTE_INFO_ORANGE}500M", f"{FLAM3H_ICON_COPY_PASTE_INFO_ORANGE}750M", f"{FLAM3H_ICON_COPY_PASTE_INFO_ORANGE}1 Billion", "" )
+            menuitems = ( "", "1M", "2M", "5M", "15M", "25M", "50M", "100M", "150M", "250M", f"{FLAM3H_ICON_STAR_HIGH_TIER}500M", f"{FLAM3H_ICON_STAR_HIGH_TIER}750M", f"{FLAM3H_ICON_STAR_HIGH_TIER}1 Billion", "" )
         else:
             menuitems = ("Please, add at least one iterator", "")
         for i, item in enumerate(menuitems):
