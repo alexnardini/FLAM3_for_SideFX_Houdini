423b2b64c2795410b944f4ecd883809f1920c0f6
Alessandro Nardini <alexnardini@gmail.com>
2024-09-26 10:31:47 +0200

    Renamed a global variable

--- src/py/py_flam3.py
+++ src/py/py_flam3.py
@@ -282,7 +282,7 @@ FLAM3H_ICON_STAR_FLAME_VAR_ACTV_OVER_ONE = '![opdef:/alexnardini::Sop/FLAM3H?ico
 FLAM3H_ICON_STAR_FLAME_VAR_ACTV_NEGATIVE = '![opdef:/alexnardini::Sop/FLAM3H?iconStarSwapCyanSVG.svg]'
 FLAM3H_ICON_STAR_FLAME_ITER_ACTV = '![opdef:/alexnardini::Sop/FLAM3H?icon_optionStarYellowOrangeSVG.svg]'
 # high tier
-FLAM3H_ICON_COPY_PASTE_INFO_RED = '![opdef:/alexnardini::Sop/FLAM3H?icon_optionStarRedSVG.svg]'
+FLAM3H_ICON_STAR_FLAME_RED = '![opdef:/alexnardini::Sop/FLAM3H?icon_optionStarRedSVG.svg]'
 
 
 
@@ -3173,7 +3173,7 @@ iterator_keep_last_weight(self) -> None:
         menu=[]
         menuitems = ()
         if iterators:
-            menuitems = ( "", "1M", "2M", "5M", "15M", "25M", "50M", "100M", "150M", "250M", f"{FLAM3H_ICON_STAR_FLAME_VAR_ACTV_OVER_ONE}500M", f"{FLAM3H_ICON_STAR_FLAME_VAR_ACTV_OVER_ONE}750M", f"{FLAM3H_ICON_COPY_PASTE_INFO_RED}1 Billion", "" )
+            menuitems = ( "", "1M", "2M", "5M", "15M", "25M", "50M", "100M", "150M", "250M", f"{FLAM3H_ICON_STAR_FLAME_VAR_ACTV_OVER_ONE}500M", f"{FLAM3H_ICON_STAR_FLAME_VAR_ACTV_OVER_ONE}750M", f"{FLAM3H_ICON_STAR_FLAME_RED}1 Billion", "" )
         else:
             menuitems = ("Please, add at least one iterator", "")
         for i, item in enumerate(menuitems):
