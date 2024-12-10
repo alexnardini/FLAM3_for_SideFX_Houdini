55926f8ee407611ec6502541acf298bdaeb0f0d0
Alessandro Nardini <alexnardini@gmail.com>
2024-12-10 14:19:48 +0100

    back to local evaluation node path

--- src/cvex/genome.h
+++ src/cvex/genome.h
@@ -122,120 +122,120 @@ struct gemPrm{
                 else if(find( {27, 28, 29, 30, 31, 32, 34, 35, 36, 37, 38, 47, 48, 49, 50, 51, 52, 53, 56, 57, 61, 63} , T )>=0){
                     if(T<39){
                         // 27 CURL
-                        if(T==27){ curl_c[i] = chu(concat("curlc_", idx)); continue; }
+                        if(T==27){ curl_c[i] = chu(concat("../curlc_", idx)); continue; }
                         // 28 NGON
-                        else if(T==28){ ngon[i]  = chp(concat("ngon_", idx)); continue; }
+                        else if(T==28){ ngon[i]  = chp(concat("../ngon_", idx)); continue; }
                         // 29 PDJ
-                        else if(T==29){ pdj_w[i] = chp(concat("pdjw_", idx)); continue; }
+                        else if(T==29){ pdj_w[i] = chp(concat("../pdjw_", idx)); continue; }
                         // 30 BLOB
-                        else if(T==30){ blob[i]  = chv(concat("blob_", idx)); continue; }
+                        else if(T==30){ blob[i]  = chv(concat("../blob_", idx)); continue; }
                         // 31 JuliaN
-                        else if(T==31){ julian[i] = chu(concat("julian_", idx)); continue; }
+                        else if(T==31){ julian[i] = chu(concat("../julian_", idx)); continue; }
                         // 32 JuliaScope
-                        else if(T==32){ juliascope[i] = chu(concat("juliascope_", idx)); continue; }
+                        else if(T==32){ juliascope[i] = chu(concat("../juliascope_", idx)); continue; }
                         // 34 FAN2
-                        else if(T==34){ fan2[i] = chu(concat("fan2_", idx)); continue; }
+                        else if(T==34){ fan2[i] = chu(concat("../fan2_", idx)); continue; }
                         // 35 RINGS2
-                        else if(T==35){ rings2_val[i] = chf(concat("rings2val_", idx)); continue; }
+                        else if(T==35){ rings2_val[i] = chf(concat("../rings2val_", idx)); continue; }
                         // 36 RECTANGLES
-                        else if(T==36){ rectangles[i] = chu(concat("rectangles_", idx)); continue; }
+                        else if(T==36){ rectangles[i] = chu(concat("../rectangles_", idx)); continue; }
                         // 37 RADIAL BLUR
-                        else if(T==37){ radialblur[i] = chf(concat("radialblur_", idx)); continue; }
+                        else if(T==37){ radialblur[i] = chf(concat("../radialblur_", idx)); continue; }
                         // 38 PIE
-                        else if(T==38){ pie[i] = chv(concat("pie_", idx)); continue; }
+                        else if(T==38){ pie[i] = chv(concat("../pie_", idx)); continue; }
                     }
                     else{
                         // 47 DISC2
                         if(T==47){
                             //( This seem to be the only one to benefit from the precalc: 18% faster. )
-                            disc2[i] = chu(concat("disc2_", idx));
+                            disc2[i] = chu(concat("../disc2_", idx));
                             float rot=disc2[i][0]; float twist=disc2[i][1];
                             vector calc; precalc_V_DISC2(calc, rot, twist);
                             pc_DISC2[i] = calc;
                             continue; }
                         // 48 SUPERSHAPE
                         else if(T==48){
-                            supershape[i]   = chv(concat("supershape_", idx));
-                            supershape_n[i] = chv(concat("supershapen_", idx)); continue; }
+                            supershape[i]   = chv(concat("../supershape_", idx));
+                            supershape_n[i] = chv(concat("../supershapen_", idx)); continue; }
                         // 49 FLOWER
-                        else if(T==49){ flower[i] = chu(concat("flower_", idx)); continue; }
+                        else if(T==49){ flower[i] = chu(concat("../flower_", idx)); continue; }
                         // 50 CONIC
-                        else if(T==50){ conic[i] = chu(concat("conic_", idx)); continue; }
+                        else if(T==50){ conic[i] = chu(concat("../conic_", idx)); continue; }
                         // 51 PARABOLA
-                        else if(T==51){ parabola[i] = chu(concat("parabola_", idx)); continue; }
+                        else if(T==51){ parabola[i] = chu(concat("../parabola_", idx)); continue; }
                         // 52 BENT2
-                        else if(T==52){ bent2[i] = chu(concat("bent2xy_", idx)); continue; }
+                        else if(T==52){ bent2[i] = chu(concat("../bent2xy_", idx)); continue; }
                         // 53 BIPOLAR
-                        else if(T==53){ bipolar_shift[i] = chf(concat("bipolarshift_", idx)); continue; }
+                        else if(T==53){ bipolar_shift[i] = chf(concat("../bipolarshift_", idx)); continue; }
                         // 56 CELL
-                        else if(T==56){ cell_size[i] = chf(concat("cellsize_", idx)); continue; }
+                        else if(T==56){ cell_size[i] = chf(concat("../cellsize_", idx)); continue; }
                         // 57 CPOW
-                        else if(T==57){ cpow[i] = chv(concat("cpow_", idx)); continue; }
+                        else if(T==57){ cpow[i] = chv(concat("../cpow_", idx)); continue; }
                         // 61 ESCHER
-                        else if(T==61){ escher_beta[i] = chf(concat("escherbeta_", idx)); continue; }
+                        else if(T==61){ escher_beta[i] = chf(concat("../escherbeta_", idx)); continue; }
                         // 63 LAZYSUSAN
                         else if(T==63){
-                            lazysusanxyz[i] = chu(concat("lazysusanxyz_", idx));
-                            lazysusan[i]    = chv(concat("lazysusan_", idx)); continue; }
+                            lazysusanxyz[i] = chu(concat("../lazysusanxyz_", idx));
+                            lazysusan[i]    = chv(concat("../lazysusan_", idx)); continue; }
                     }
                 }
                 else if(find( {66, 67, 69, 71, 72, 73, 74, 75, 76, 77, 78, 79, 94, 95, 96, 97, 98, 99, 101, 102, 103, 105} , T )>=0){
                     if(T<79){
                         // 66 MODULUS
-                        if(T==66){ modulus[i] = chu(concat("modulusXYZ_", idx)); continue; }
+                        if(T==66){ modulus[i] = chu(concat("../modulusXYZ_", idx)); continue; }
                         // 67 OSCOPE
-                        else if(T==67){ oscope[i] = chp(concat("oscope_", idx)); continue; }
+                        else if(T==67){ oscope[i] = chp(concat("../oscope_", idx)); continue; }
                         // 69 POPCORN2
                         else if(T==69){
-                            popcorn2[i]   = chu(concat("popcorn2xyz_", idx));
-                            popcorn2_c[i] = chf(concat("popcorn2c_", idx)); continue; }
+                            popcorn2[i]   = chu(concat("../popcorn2xyz_", idx));
+                            popcorn2_c[i] = chf(concat("../popcorn2c_", idx)); continue; }
                         // 71 SEPARATION
                         else if(T==71){
-                            separation[i]        = chu(concat("separationxyz_", idx));
-                            separation_inside[i] = chu(concat("separationinsidexyz_", idx)); continue; }
+                            separation[i]        = chu(concat("../separationxyz_", idx));
+                            separation_inside[i] = chu(concat("../separationinsidexyz_", idx)); continue; }
                         // 72 SPLIT
-                        else if(T==72){ split[i] = chu(concat("splitxyz_", idx)); continue; }
+                        else if(T==72){ split[i] = chu(concat("../splitxyz_", idx)); continue; }
                         // 73 SPLITS
-                        else if(T==73){ splits[i] = chu(concat("splitsxyz_", idx)); continue; }
+                        else if(T==73){ splits[i] = chu(concat("../splitsxyz_", idx)); continue; }
                         // 74 STRIPES
-                        else if(T==74){ stripes[i] = chu(concat("stripes_", idx)); continue; }
+                        else if(T==74){ stripes[i] = chu(concat("../stripes_", idx)); continue; }
                         // 75 WEDGE
-                        else if(T==75){ wedge[i] = chp(concat("wedge_", idx)); continue; }
+                        else if(T==75){ wedge[i] = chp(concat("../wedge_", idx)); continue; }
                         // 76 WEDGE JULIA
-                        else if(T==76){ wedgejulia[i] = chp(concat("wedgejulia_", idx));
+                        else if(T==76){ wedgejulia[i] = chp(concat("../wedgejulia_", idx));
                             // precalc ( The precalc made it 10% slower, I dnt know why...odd. )
                             // float power=wedgejulia[i][0]; float angle= wedgejulia[i][1]; float dist=wedgejulia[i][2]; float count=wedgejulia[i][3];
                             // vector calc; precalc_V_WEDGEJULIA(calc, power, angle, dist, count);
                             // pc_WEDGEJULIA[i] = calc;
                             continue; }
                         // 77 WEDGE SPH
-                        else if(T==77){ wedgesph[i] = chp(concat("wedgesph_", idx)); continue; }
+                        else if(T==77){ wedgesph[i] = chp(concat("../wedgesph_", idx)); continue; }
                         // 78 WHORL
-                        else if(T==78){ whorl[i]  = chu(concat("whorl_", idx)); continue; }
+                        else if(T==78){ whorl[i]  = chu(concat("../whorl_", idx)); continue; }
                     }
                     else{
                         // 79 WAVES2
                         if(T==79){
-                            waves2_scale[i] = chu(concat("waves2scalexyz_", idx));
-                            waves2_freq[i]  = chu(concat("waves2freqxyz_", idx)); continue; }
+                            waves2_scale[i] = chu(concat("../waves2scalexyz_", idx));
+                            waves2_freq[i]  = chu(concat("../waves2freqxyz_", idx)); continue; }
                         // 94 AUGER
-                        else if(T==94){ auger[i] = chp(concat("auger_", idx)); continue; }
+                        else if(T==94){ auger[i] = chp(concat("../auger_", idx)); continue; }
                         // 95 FLUX
-                        else if(T==95){ flux_spread[i] = chf(concat("fluxspread_", idx)); continue; }
+                        else if(T==95){ flux_spread[i] = chf(concat("../fluxspread_", idx)); continue; }
                         // 96 MOBIUS
                         else if(T==96){
-                            mobius_re[i] = chp(concat("mobiusre_", idx));
-                            mobius_im[i] = chp(concat("mobiusim_", idx)); continue; }
+                            mobius_re[i] = chp(concat("../mobiusre_", idx));
+                            mobius_im[i] = chp(concat("../mobiusim_", idx)); continue; }
                         // 97 CURVE
                         else if(T==97){
-                            curve_lenght[i] = chu(concat("curvexyzlenght_", idx));
-                            curve_amp[i]    = chu(concat("curvexyzamp_", idx)); continue; }
+                            curve_lenght[i] = chu(concat("../curvexyzlenght_", idx));
+                            curve_amp[i]    = chu(concat("../curvexyzamp_", idx)); continue; }
                         // 98 PERSPECTIVE
-                        else if(T==98){ persp[i] = chu(concat("persp_", idx)); continue; }
+                        else if(T==98){ persp[i] = chu(concat("../persp_", idx)); continue; }
                         // 99 BWRAPS
                         else if(T==99){
-                            bwraps[i] = chv(concat("bwraps_", idx));
-                            bwrapstwist[i] = chu(concat("bwrapstwist_", idx));
+                            bwraps[i] = chv(concat("../bwraps_", idx));
+                            bwrapstwist[i] = chu(concat("../bwrapstwist_", idx));
                             // precalc ( The precalc made it 15% slower, I dnt know why...odd. )
                             // float cellsize=bwraps[i][0]; float space=bwraps[i][1]; float gain=bwraps[i][2];
                             // vector calc; precalc_V_BWRAPS(calc, cellsize, space, gain);
@@ -243,15 +243,15 @@ struct gemPrm{
                             continue; }
                         // 101 POLYNOMIAL
                         else if(T==101){
-                            polynomial_pow[i] = chu(concat("polynomialpow_", idx));
-                            polynomial_lc[i]  = chu(concat("polynomiallc_", idx));
-                            polynomial_sc[i]  = chu(concat("polynomialsc_", idx)); continue; }
+                            polynomial_pow[i] = chu(concat("../polynomialpow_", idx));
+                            polynomial_lc[i]  = chu(concat("../polynomiallc_", idx));
+                            polynomial_sc[i]  = chu(concat("../polynomialsc_", idx)); continue; }
                         // 102 CROP
                         else if(T==102){
-                            crop_ltrb[i] = chp(concat("cropltrb_", idx));
-                            crop_az[i] = chu(concat("cropaz_", idx)); continue; }
+                            crop_ltrb[i] = chp(concat("../cropltrb_", idx));
+                            crop_az[i] = chu(concat("../cropaz_", idx)); continue; }
                         // 105 POINT SYMMETRY
-                        else if(T==105){ point_symmetry[i] = chv(concat("ptsym_", idx)); continue; }
+                        else if(T==105){ point_symmetry[i] = chv(concat("../ptsym_", idx)); continue; }
                     }
                 }
             }
