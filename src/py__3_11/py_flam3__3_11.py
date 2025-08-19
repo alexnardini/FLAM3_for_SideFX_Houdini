f1d2029f7eae4b74720da53575226a15c1912b97
Alessandro Nardini <alexnardini@gmail.com>
2025-08-19 14:00:53 +0200

    Updated doc-string for flam3h_iterator_utils -> def update_xml_last_loaded(self) -> None:

--- src/py__3_11/py_flam3__3_11.py
+++ src/py__3_11/py_flam3__3_11.py
@@ -5421,7 +5421,37 @@ class flam3h_iterator_utils
     def update_xml_last_loaded(self) -> None:
         """Force node user data "XML_last_loaded" to update.
         It will update only if: xml and xml_isFile and xml == xml_history and inisvalidfile and inisvalidpreset and not clipboard
-
+        
+        This definition is being run in several places to try to catch a change sooner than later.
+        
+        Below is a list:
+        
+            - flam3h_scripts.flam3h_on_loaded(self) -> None:
+            - flam3h_scripts.flam3h_on_deleted(self) -> None:
+            - flam3h_general_utils.menus_refresh_enum_prefs(self) -> None:
+            - flam3h_general_utils.flam3h_init_presets_CP_PRESETS(self, mode: int = 1, destroy_menus: bool = True, json_file: Union[bool, None] = None, f3h_json_file: Union[bool, None] = None, json_path_checked: Union[str, bool, None] = None) -> None:
+            - flam3h_general_utils.flam3h_init_presets_IN_PRESETS(self, mode: int = 1) -> None:
+            - flam3h_general_utils.flam3h_init_presets_OUT_PRESETS(self, destroy_menus: bool = True) -> None:
+            - flam3h_iterator_utils.refresh_iterator_vars_menu(self) -> None:
+            - flam3h_iterator_utils.prm_paste(self) -> None:
+            - flam3h_iterator_utils.prm_paste_FF_CTRL(self) -> None:
+            - flam3h_iterator_utils.prm_paste_sel_pre_affine(self) -> None:
+            - flam3h_iterator_utils.prm_paste_sel_post_affine(self) -> None:
+            - flam3h_iterator_utils.prm_paste_sel_pre_affine_FF(self) -> None:
+            - flam3h_iterator_utils.prm_paste_sel_post_affine_FF(self) -> None:
+            - flam3h_iterator_utils.flam3h_xaos_convert(self) -> None:
+            - flam3h_iterator_utils.swap_iter_pre_vars(self) -> None:
+            - flam3h_iterator_utils.iterators_count(self) -> None:
+            - flam3h_palette_utils.flam3h_ramp_save(self) -> None:
+            - flam3h_palette_utils.json_to_flam3h_ramp_SET_PRESET_DATA(self, node: hou.SopNode) -> None:
+            - flam3h_palette_utils.json_to_flam3h_ramp(self, use_kwargs: bool = True) -> None:
+            - flam3h_ui_msg_utils.ui_xaos_infos(self) -> None:
+            - in_flame_utils.in_copy_render_all_stats_msg(kwargs: dict,  apo_data: Union[in_flame_iter_data, None] = None, clipboard: bool = False, flash_message: bool = False) -> None:
+            - in_flame_utils.in_copy_sensor_stats_msg(kwargs: dict) -> None:
+            - in_flame_utils.in_copy_render_stats_msg(kwargs: dict) -> None:
+            - out_flame_utils.reset_OUT_kwargs(self) -> None:
+            - out_flame_utils.out_XML(self) -> None:
+            
         Args:
             (self):
             
