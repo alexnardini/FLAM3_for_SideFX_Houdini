45a5b0536b69a05a63843bf0bd3d8ddb88f09dc5
Alessandro Nardini <alexnardini@gmail.com>
2025-09-09 23:27:55 +0200

    Pulling the custom node info license information from the: nodetype.hdamodule().flam3.__license__(FLAM3H™) and nodetype.hdamodule().flam3usd.__license__(FLAM3HUSD)

--- src/py__custom_node_info/FLAM3H/custom_node_info.py
+++ src/py__custom_node_info/FLAM3H/custom_node_info.py
@@ -10,10 +10,11 @@ FLAM3H_NODE_TYPE_NAME_CATEGORY = 'alexnardini::Sop/FLAM3H'
 nodetype = hou_nodeType(FLAM3H_NODE_TYPE_NAME_CATEGORY)
 __version__ = nodetype.hdaModule().__version__
 __status__  = nodetype.hdaModule().__status__
+__license__ = nodetype.hdaModule().flam3.__license__
 
 # Build custom node info
 addLabeledText("Description", "The Fractal Flame Algorithm: FLAM3 - Create fractal art in Houdini")
 addLabeledText("Copyright", "© 2021 F stands for liFe (made in Italy)")
 addLabeledText("Version", f"{__version__} - {__status__}")
-addLabeledText("Lic", "GPL v3.0")
+addLabeledText("Lic", __license__)
 
--- src/py__custom_node_info/FLAM3HUSD/custom_node_info.py
+++ src/py__custom_node_info/FLAM3HUSD/custom_node_info.py
@@ -10,9 +10,10 @@ FLAM3HUSD_NODE_TYPE_NAME_CATEGORY = 'alexnardini::Lop/FLAM3HUSD'
 nodetype = hou_nodeType(FLAM3HUSD_NODE_TYPE_NAME_CATEGORY)
 __version__ = nodetype.hdaModule().__version__
 __status__  = nodetype.hdaModule().__status__
+__license__ = nodetype.hdaModule().flam3usd.__license__
 
 # Build custom node info
 addLabeledText("Description", "Render FLAM3H™ fractal Flames in Solaris using Karma")
 addLabeledText("Copyright", "© 2023 F stands for liFe (made in Italy)")
 addLabeledText("Version", f"{__version__} - {__status__}")
-addLabeledText("Lic", "GPL v3.0")
+addLabeledText("Lic", __license__)
