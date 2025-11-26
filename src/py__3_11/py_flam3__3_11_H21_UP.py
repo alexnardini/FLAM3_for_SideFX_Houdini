bb5d03535fb54286761f8b7b2222143168cd2baf
Alessandro Nardini <alexnardini@gmail.com>
2025-11-26 19:22:57 +0100

    F3H-50 checks against the str.lower() inside: def remove_locked_from_flame_stats(node) -> None:

--- src/py__3_11/py_flam3__3_11_H21_UP.py
+++ src/py__3_11/py_flam3__3_11_H21_UP.py
@@ -2991,7 +2991,8 @@ class flam3h_general_utils
         """  
         stats: str = node.parm(MSG_IN_FLAMESTATS).eval()
         lines: list[str] = stats.splitlines()
-        if str(lines[0]).startswith(MSG_FLAMESTATS_LOCK): lines[0] = ''
+        # Just in case lets check the str.lower()
+        if str(lines[0]).lower().startswith(MSG_FLAMESTATS_LOCK.__str__().lower()): lines[0] = ''
         stats_join: str = "\n".join(lines)
         node.parm(MSG_IN_FLAMESTATS).set(stats_join)
 
