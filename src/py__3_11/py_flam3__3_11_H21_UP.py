ebddb8c4e96d2a41d6db918eb07301f5d90562c7
Alessandro Nardini <alexnardini@gmail.com>
2026-05-06 19:13:11 +0200

    F3H-184 Updated

--- src/py__3_11/py_flam3__3_11_H21_UP.py
+++ src/py__3_11/py_flam3__3_11_H21_UP.py
@@ -96,7 +96,7 @@ __h_version_max__: int = nodetype.hdaModule().__h_version_max__
 
     Title:      FLAM3H™ H21 UP. SideFX Houdini FLAM3: PYTHON
     Author:     F stands for liFe ( made in Italy )
-    date:       August 2025, Last revised April 2026 (cloned from: py_flam3__3_11.py)
+    date:       August 2025, Last revised May 2026 (cloned from: py_flam3__3_11.py)
                 Source file start date: April 2025
 
     Name:       PY_FLAM3__3_11_H21_UP "PYTHON" ( The ending filename digits represent the least python version needed to run this code )
@@ -8291,7 +8291,7 @@ class flam3h_iterator_utils
                 flam3h_general_utils(self.kwargs).flam3h_toggle_mp_xf_viz_solo_follow(str(preset_id))
                 
                 n: flam3h_iterator_prm_names = flam3h_iterator_prm_names()
-                _MSG: str = f"iterator: {preset_id}"
+                _MSG: str = f"{preset_id}"
                 active: int = node.parm(f"{n.main_vactive}_{preset_id}").eval()
                 weight: float = node.parm(f"{n.main_weight}_{preset_id}").eval()
                 
@@ -11034,36 +11034,35 @@ class flam3h_iterator_utils
                 flam3h_general_utils(kwargs).flam3h_toggle_mp_xf_viz_solo_follow(str(mp_id))
                 
                 # Probably we can avoid to do all of this if the user is just switching tabs directly as it can be annoying...?
-                '''
-                from_FLAM3H_NODE, mp_id_from, isDELETED = self.prm_paste_update_for_undo(node)
                 
-                n: flam3h_iterator_prm_names = flam3h_iterator_prm_names()
-                _MSG: str = f"iterator: {mp_id}"
-                active: int = node.parm(f"{n.main_vactive}_{mp_id}").eval()
-                weight: float = node.parm(f"{n.main_weight}_{mp_id}").eval()
+                # from_FLAM3H_NODE, mp_id_from, isDELETED = self.prm_paste_update_for_undo(node)
                 
-                if node == from_FLAM3H_NODE and mp_id_from == mp_id:
-                    if active and weight > 0:
-                        flam3h_general_utils.flash_message(node, f"{_MSG} (Marked)")
-                        flam3h_general_utils.set_status_msg(f"{node.name()}: {_MSG} (Marked)", 'MSG')
-                    elif active and weight == 0:
-                        flam3h_general_utils.flash_message(node, f"{_MSG} (Zero Weight and Marked)")
-                        flam3h_general_utils.set_status_msg(f"{node.name()}: {_MSG} (Zero Weight and Marked)", 'MSG')
-                    else:
-                        flam3h_general_utils.flash_message(node, f"{_MSG} (Disabled and Marked)")
-                        flam3h_general_utils.set_status_msg(f"{node.name()}: {_MSG} (Disabled and Marked)", 'MSG')
+                # n: flam3h_iterator_prm_names = flam3h_iterator_prm_names()
+                # _MSG: str = f"{mp_id}"
+                # active: int = node.parm(f"{n.main_vactive}_{mp_id}").eval()
+                # weight: float = node.parm(f"{n.main_weight}_{mp_id}").eval()
+                
+                # if node == from_FLAM3H_NODE and mp_id_from == mp_id:
+                #     if active and weight > 0:
+                #         flam3h_general_utils.flash_message(node, f"{_MSG} (Marked)")
+                #         flam3h_general_utils.set_status_msg(f"{node.name()}: {_MSG} (Marked)", 'MSG')
+                #     elif active and weight == 0:
+                #         flam3h_general_utils.flash_message(node, f"{_MSG} (Zero Weight and Marked)")
+                #         flam3h_general_utils.set_status_msg(f"{node.name()}: {_MSG} (Zero Weight and Marked)", 'MSG')
+                #     else:
+                #         flam3h_general_utils.flash_message(node, f"{_MSG} (Disabled and Marked)")
+                #         flam3h_general_utils.set_status_msg(f"{node.name()}: {_MSG} (Disabled and Marked)", 'MSG')
                     
-                else:
-                    if active and weight > 0:
-                        flam3h_general_utils.flash_message(node, _MSG)
-                        flam3h_general_utils.set_status_msg(f"{node.name()}: {_MSG}", 'MSG')
-                    elif active and weight == 0:
-                        flam3h_general_utils.flash_message(node, f"{_MSG} (Zero Weight)")
-                        flam3h_general_utils.set_status_msg(f"{node.name()}: {_MSG} (Zero Weight)", 'MSG')
-                    else:
-                        flam3h_general_utils.flash_message(node, f"{_MSG} (Disabled)")
-                        flam3h_general_utils.set_status_msg(f"{node.name()}: {_MSG} (Disabled)", 'MSG')
-                '''                
+                # else:
+                #     if active and weight > 0:
+                #         flam3h_general_utils.flash_message(node, _MSG)
+                #         flam3h_general_utils.set_status_msg(f"{node.name()}: {_MSG}", 'MSG')
+                #     elif active and weight == 0:
+                #         flam3h_general_utils.flash_message(node, f"{_MSG} (Zero Weight)")
+                #         flam3h_general_utils.set_status_msg(f"{node.name()}: {_MSG} (Zero Weight)", 'MSG')
+                #     else:
+                #         flam3h_general_utils.flash_message(node, f"{_MSG} (Disabled)")
+                #         flam3h_general_utils.set_status_msg(f"{node.name()}: {_MSG} (Disabled)", 'MSG')
 
         else:
             # Clear menu cache
